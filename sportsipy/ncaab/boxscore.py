d28c1f19ed37165f93f072865db953617023aa5b
tuckerhschmidt <57376733+tuckerhschmidt@users.noreply.github.com>
2021-08-21 12:22:45 -0400

    Update boxscore.py
    do not get team records to avoid error.

--- sportsipy/ncaab/boxscore.py
+++ sportsipy/ncaab/boxscore.py
@@ -680,7 +680,8 @@ class Boxscore:
                 index = BOXSCORE_ELEMENT_INDEX[short_field]
             if short_field == 'away_record' or \
                short_field == 'home_record':
-                value = self._parse_record(short_field, boxscore, index)
+                #value = self._parse_record(short_field, boxscore, index)
+                value = '0-0'
                 setattr(self, field, value)
                 continue
             value = utils._parse_field(BOXSCORE_SCHEME,
